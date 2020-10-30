51eb6656a0a0ae81116ddb42776e33722c3bc747
Mathilde Congiu <mathilde.congiu@hotmail.com>
2020-10-30 11:50:41 +0100

    Add configs folder. Add signup within index. Add User model. Login and signup views

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    level: {
+      name: {
+        type: String,
+        enum: [
+          "Planetkiller",
+          "Eco-survivor",
+          "Green Explorador",
+          "Eco-Warrior",
+          "PlanetSaviour",
+        ],
+        default: "Planetkiller",
+      },
+      rank: { type: Number, enum: [1, 2, 3, 4, 5], default: 1 },
+      img: {
+        type: String,
+        enum: [
+          "images/planetkiller.png",
+          "images/eco-survivor.png",
+          "images/green-explorador.png",
+          "images/eco-warrior.png",
+          "images/planetsaviour.png",
+        ],
+        default: "images/planetkiller.png",
+      },
+    },
+
+    experience: { type: Number, default: 0 },
+    actions: [mongoose.Types.ObjectID],
+  },
+  { 
+      timestamps: {
+           createdAt: "created_at", 
+           updatedAt: "updated_at" 
+        } }
+);
+
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. Add redirection to Signup, or Dashboard if logged iN*/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup');
+}
+)
 
 module.exports = router;
