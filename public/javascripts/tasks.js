8be93a22239a6bca7545c1c01264ceb1c3b5218b
Alex Olle <alex.olle.93@gmail.com>
2020-11-04 17:11:42 +0100

    First animations

---
+++ public/javascripts/tasks.js
@@ -0,0 +1,14 @@
+console.log("Hola")
+
+let checkbox = document.getE
+
+function taskDone() {
+    var x = document.getElementsByClassName("one-task");
+    console.log("hola")
+    
+    // if (x.className === "nav-bar") {
+    //   x.className += " responsive";
+    // } else {
+    //   x.className = "nav-bar";
+    // }
+} 
